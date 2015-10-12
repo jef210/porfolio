eb53c8dd4c967614cb7d748be28e0d05199621dc
jef210 <jmalek210@gmail.com>
2015-10-12 12:27:49 -0600

    added hide(), messing w/colors

--- main.js
+++ main.js
@@ -2,9 +2,35 @@
 
 $(document).ready(function(){
     // Toggle the section content 
-    $('.content-click').on('click', function(){
-        $('#click-text').slideToggle();
+    $('#about-text').hide();
+    $('.about-section').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('#about-text').fadeToggle('slow');
     });
+    
+    $('#examples-text').hide();
+    $('.examples-section').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('#examples-text').fadeToggle();
+    }); 
+    
+    $('#what-i-do-text').hide();
+    $('.what-i-do-section').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('#what-i-do-text').fadeToggle();
+    }); 
+    
+    $('#other-stuff-text').hide();
+    $('.other-stuff-section').on('click', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('#other-stuff-text').fadeToggle();
+    }); 
+
+
 });
 
 })();
